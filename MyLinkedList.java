c794687dcd4ce1e53dc3323be6a159061103b2fb
ellakrechmer <ekrechmer20@stuy.edu>
2021-01-05 17:00:30 -0500

    created get method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -56,7 +56,13 @@ public class MyLinkedList{
      return true;
    }
  }
- //public String get(int index);
+ public String get(int index){
+   Node curr=start;
+   for (int i=0; i<index; i++){
+     curr=curr.getNext();
+   }
+   return curr.getData();
+ }
  //public String set(int index, String value);
  //public String toString();
  //Any helper method that returns a Node object MUST BE PRIVATE!
