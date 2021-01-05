39c669ba459f4d2ce4333c5965ca55521aecbf75
ellakrechmer <ekrechmer20@stuy.edu>
2021-01-05 16:44:40 -0500

    created add (to end) method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,11 +7,23 @@ public class MyLinkedList{
  public int size() {
    return size;
  }
- 
- public boolean add(String value);
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+ public boolean add(String value){
+   Node n=new Node(value);
+   if (start==null){
+     start=n;
+     end=n;
+     size++;
+   }
+   else{
+     n.setPrev(end);
+     end.setNext(n);
+     size++; 
+   }
+   return true;
+ }
+ //public boolean add(int index, String value);
+ //public String get(int index);
+ //public String set(int index, String value);
+ //public String toString();
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
