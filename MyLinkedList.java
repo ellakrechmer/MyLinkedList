29282eb4a865669fc351a574a88eea83bb490cce
ellakrechmer <ekrechmer20@stuy.edu>
2021-01-05 16:58:53 -0500

    created second add method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,9 +7,10 @@ public class MyLinkedList{
  public int size() {
    return size;
  }
+
  public boolean add(String value){
    Node n=new Node(value);
-   if (start==null){
+   if (size()==0){
      start=n;
      end=n;
      size++;
@@ -17,11 +18,44 @@ public class MyLinkedList{
    else{
      n.setPrev(end);
      end.setNext(n);
-     size++; 
+     size++;
    }
    return true;
  }
- //public boolean add(int index, String value);
+ public boolean add(int index, String value) {
+   Node n=new Node(value);
+   if (size()==0) {
+     start=n;
+     end=n;
+     size++;
+     return true;
+   }
+   else if (index==0) {
+     n.setNext(start);
+     start.setPrev(n);
+     start=n;
+     size++;
+     return true;
+   }
+   else if (index==size()) {
+     add(value);
+     return true;
+   }
+   else{
+     Node curr=start;
+     for (int i=0; i<index; i++){
+       curr=curr.getNext();
+     }
+     Node next=curr.getNext();
+     Node prev=curr.getPrev();
+     n.setPrev(next.getPrev());
+     n.setNext(prev.getNext());
+     prev.setNext(n);
+     next.setPrev(n);
+     size++;
+     return true;
+   }
+ }
  //public String get(int index);
  //public String set(int index, String value);
  //public String toString();
